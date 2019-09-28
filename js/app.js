9399a0c44c84522908ba51029ba706609bb2afe0
Christopher Stephen Barnard <info@evakos.io>
2019-09-28 21:54:36 +0300

    Chart on delete values

--- js/app.js
+++ js/app.js
@@ -1,59 +1,70 @@
 // Expense Constructor essential but still not sure why.
 
 let idcounter = 0;
+
 function Expense(type, amount) {
+
   this.type = type;
+
   this.amount = amount;
+
   this.id = idcounter++;
+
 }
 
-//Global Array
+//Global Vars
 
 let storeAmounts = [];
 
-/// Nedd this exaplining still
+let budget = 1600; //We will get this on input in the UI.
+
+let budgetPercent = (budget / budget) * 100; //Return as a percent dont really need this.
+
+/// Need this exaplining still
 function add(accumulator, a) {
+
   return accumulator + a.amount;
+
 }
 
-//Chart Constructor
-//function Chart() {}
+//function Chart() { }
 
 const ctx = document.getElementById('chart');
 
-const expenseChart = new Chart(ctx, {
+let expenseChart = new Chart(ctx, {
+
   type: 'doughnut',
   data: {
-    labels: ['Percent of Budget', 'Budget'],
     datasets: [
       {
-        data: [30, 70],
-        backgroundColor: ['rgba(255, 99, 132, 1)', 'rgba(54, 162, 235, 1']
+        backgroundColor: ['rgba(0, 0, 0, .05)', 'rgba(0, 0, 0, .05)'],
+        data: [100, 100]
       }
-    ]
+    ],
+
+    borderColor: ['rgba(233, 255, 255, 1)']
   },
   options: {
-    legend: {
-      display: false
+    animation: {
+      duration: 1000,
+      easing: 'linear'
     }
   }
 });
 
-console.log(expenseChart);
+//Empty UI Constructor
 
-// UI Constructor
-
-function UI() {}
+function UI() { }
 
 // Add Expense To List:
 
-UI.prototype.addExpenseToList = function(expense) {
+UI.prototype.addExpenseToList = function (expense) {
+
   const list = document.getElementById('expense-list');
 
   const row = document.createElement('tr');
 
-  //Where is the id being added though. Not physically.
-  row.expenseid = expense.id;
+  row.expenseid = expense.id;  // Where is the id being added though. Not physically.
 
   row.innerHTML = `
     
@@ -61,65 +72,94 @@ UI.prototype.addExpenseToList = function(expense) {
 
     <td>${expense.amount}</td>
 
-    <td>${expense.id}</td>
-
     `;
 
   list.appendChild(row);
 };
 
 // Show Alert
-UI.prototype.showAlert = function(message, className) {
+
+UI.prototype.showAlert = function (message, className) {
+
   const div = document.createElement('div');
 
   document.querySelector('.messages').appendChild(div);
 
   div.appendChild(document.createTextNode(message));
 
-  // Add classes
-  div.className = `alert ${className}`;
+  div.className = `alert ${className}`;// Add classes
 
-  // Timeout after 3 sec
-  setTimeout(function() {
+  setTimeout(function () { // Timeout after 3 sec
+
     document.querySelector('.alert').remove();
+
   }, 2500);
+
 };
 
 // Clear Fields
 
-UI.prototype.clearFields = function() {
+UI.prototype.clearFields = function () {
+
   document.getElementById('type').value = '';
+
   document.getElementById('amount').value = '';
 };
 
+
 // Delete Entry
 
-UI.prototype.deleteExpense = function(target) {
+UI.prototype.deleteExpense = function (target) {
+
   let current = target;
+
   while (current.tagName != 'TR') {
+
     current = current.parentElement;
+
   }
 
-  console.log(current.expenseid);
+  //console.log(current.expenseid);
 
   for (let i = storeAmounts.length - 1; i >= 0; i--) {
+
     let el = storeAmounts[i];
+
     if (el.id == current.expenseid) {
+
       storeAmounts.splice(i, 1);
+
     }
+
   }
+
   const sum = storeAmounts.reduce(add, 0); //Initalise
 
   document.getElementById('sum').textContent = sum;
 
   current.remove();
+
+
+
+
+  let sum1 = (sum / budget) * 100; //Expenense as a percentage of the budget
+
+  let sum2 = budgetPercent + sum1;
+
+  expenseChart.data.datasets[0].data = [sum1, sum2];
+
+  expenseChart.data.datasets[0].backgroundColor = ['rgba(255, 102, 153, 1)'];
+
+  expenseChart.update();
+
 };
 
 // Event Listener for add expense
-document.getElementById('expense-form').addEventListener('submit', function(e) {
+
+document.getElementById('expense-form').addEventListener('submit', function (e) {
+
   // Get form values, don't need to repeat the const.
-  const type = document.getElementById('type').value,
-    amount = document.getElementById('amount').value;
+  const type = document.getElementById('type').value, amount = document.getElementById('amount').value;
 
   // Instantiate expense this is from the expense object in line 1
   const expense = new Expense(type, amount);
@@ -127,8 +167,11 @@ document.getElementById('expense-form').addEventListener('submit', function(e) {
   let amountNo = parseInt(amount);
 
   let expenseObj = {
+
     amount: amountNo,
+
     id: expense.id
+
   };
 
   storeAmounts.push(expenseObj);
@@ -138,6 +181,18 @@ document.getElementById('expense-form').addEventListener('submit', function(e) {
   const sum = storeAmounts.reduce(add, 0);
 
   document.getElementById('sum').textContent = sum;
+
+  // Update Chart
+
+  let sum1 = (sum / budget) * 100; //Expenense as a percentage of the budget
+
+  let sum2 = budgetPercent - sum1;
+
+  expenseChart.data.datasets[0].data = [sum1, sum2];
+
+  expenseChart.data.datasets[0].backgroundColor = ['rgba(255, 102, 153, 1)'];
+
+  expenseChart.update();
 
   // Instantiate UI
 
@@ -149,27 +204,32 @@ document.getElementById('expense-form').addEventListener('submit', function(e) {
     document.querySelector('.messages').style.backgroundColor = 'salmon';
     // Error alert
     ui.showAlert(' Please fill in all fields', 'error');
+
   } else {
+
     // Add expense to list
     ui.addExpenseToList(expense);
 
     // Show success
     document.querySelector('.messages').style.backgroundColor = '#c5e2d9';
+
     ui.showAlert(' Expense Added', 'success');
 
     // Clear fields
     ui.clearFields();
   }
 
-  console.log(storeAmounts);
+  //console.log(storeAmounts);
 
   e.preventDefault();
+
 });
 
-// Output Total
+
 
 // Event Listener for delete
-document.getElementById('expense-list').addEventListener('click', function(e) {
+
+document.getElementById('expense-list').addEventListener('click', function (e) {
   // Instantiate UI
   const ui = new UI();
 
@@ -185,41 +245,63 @@ document.getElementById('expense-list').addEventListener('click', function(e) {
   e.preventDefault();
 });
 
+
 // Populate Drop Down
 
 let dropdown = document.getElementById('type');
+
 dropdown.length = 0;
 
 let defaultOption = document.createElement('option');
+
 defaultOption.text = 'Enter Expense Type';
+
 defaultOption.value = '';
 
 dropdown.add(defaultOption);
+
 dropdown.selectedIndex = 0;
 
 const url = 'json/json';
 
 fetch(url)
-  .then(function(response) {
+
+  .then(function (response) {
+
     if (response.status !== 200) {
+
       console.warn(
+
         'Looks like there was a problem. Status Code: ' + response.status
       );
+
       return;
+
     }
 
     // Examine the text in the response
-    response.json().then(function(data) {
+
+    response.json().then(function (data) {
+
       let option;
 
       for (let i = 0; i < data.length; i++) {
+
         option = document.createElement('option');
+
         option.text = data[i].type;
+
         option.value = data[i].type;
+
         dropdown.add(option);
+
       }
+
     });
+
   })
-  .catch(function(err) {
+  .catch(function (err) {
+
     console.error('Fetch Error -', err);
+
   });
